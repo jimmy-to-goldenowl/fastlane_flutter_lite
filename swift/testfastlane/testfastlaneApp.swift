57d6bdf4885f227b4148db0d74e4e2abf1f475bf
j1mmyto9 <khanhuitse05@gmail.com>
2021-06-28 21:21:29 +0700

    up

---
+++ swift/testfastlane/testfastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  testfastlaneApp.swift
+//  testfastlane
+//
+//  Created by Ping9 on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct testfastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
